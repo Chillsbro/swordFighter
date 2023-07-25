81eca1a29f26a95cd4a5b1b09306a498c682781c
Chillsbro <114181571+Chillsbro@users.noreply.github.com>
2023-07-25 13:35:33 -0500

    add collision block class and logic to parsedCollisions for new collision block array which contains new collision block class

--- js/data/collisions.js
+++ js/data/collisions.js
@@ -11,18 +11,40 @@ const collisionsLevel1 = [
 // returning new array to then loop through and find collision block at 292 location.
 Array.prototype.parse2D = function () {
   const rows = [];
-  for (let i =0; i < this.length; i+= 16) {
-   rows.push(this.slice(i, i + 16))
-
+  for (let i = 0; i < this.length; i += 16) {
+    rows.push(this.slice(i, i + 16));
   }
   return rows;
 };
+class CollisionBlock {
+  constructor({ position }) {
+    this.position = position;
+    this.width = 64;
+    this.height = 64;
+  }
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(
+      this.position.x,
+      this.position.y,
+      this.position.width,
+      this.position.height
+    );
+  }
+}
+const collisionBlocks = [];
 
 const parsedCollisions = collisionsLevel1.parse2D();
-parsedCollisions.forEach(row => {
-  row.forEach(symbol => {
+parsedCollisions.forEach((row, y) => {
+  row.forEach((symbol) => {
     if (symbol === 292) {
       // push new collision into collision block
+      collisionBlocks.push( new CollisionBlock( {
+        position: {
+          x: 0,
+          y: y * 6,
+        }
+      }))
     }
-  })
-})
+  });
+});
