7ec390196346bf788be660f9dbdc0db8e3f1e76d
Chillsbro <114181571+Chillsbro@users.noreply.github.com>
2023-07-25 02:50:37 -0500

    moved data into js folder

--- js/data/collisions.js
+++ js/data/collisions.js
@@ -10,6 +10,11 @@ const collisionsLevel1 = [
 Array.prototype.parse2D = function () {
   const rows = [];
   for (let i =0; i < this.length; i+= 16) {
-   rows.push()
+   rows.push(this.slice(i, i + 16))
+
   }
+  return rows;
 };
+
+const parsedCollisions = collisionsLevel1.parse2D();
+console.log(parsedCollisions)
