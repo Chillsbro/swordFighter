8141154c4ba99a83952ae0c690c59507cad5db97
Chillsbro <114181571+Chillsbro@users.noreply.github.com>
2023-07-25 20:48:54 -0500

    add player to extend sprite class

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,5 +1,6 @@
-class Player {
-  constructor({ collisionBlocks = [] }) {
+class Player extends Sprite {
+  constructor({ collisionBlocks = [], imageSrc, frameRate }) {
+    super({ imageSrc, frameRate });
     this.position = {
       x: 200,
       y: 200,
@@ -10,8 +11,7 @@ class Player {
       y: 0,
     };
 
-    this.width = 25;
-    this.height = 25;
+    
     this.sides = {
       bottom: this.position.y + this.height,
     };
@@ -19,18 +19,15 @@ class Player {
     this.collisionBlocks = collisionBlocks;
   }
 
-  draw() {
-    c.fillStyle = "red";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
   update() {
+    c.fillStyle = "rgba(0, 0, 255, 0.1)";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
     this.position.x += this.velocity.x;
     // check for horizontal collisions
     this.checkForHorizontalCollisions();
     //apply gravity
     this.applyGravity();
     this.checkForVerticalCollisions();
-    
   }
   checkForHorizontalCollisions() {
     for (let i = 0; i < this.collisionBlocks.length; i++) {
